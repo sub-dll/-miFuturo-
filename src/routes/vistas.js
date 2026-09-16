456adcc3e1ea2483d1c2115ce30fe1ba58402102
sub-dll <dfonseca2024@alu.uct.cl>
2026-09-16 15:27:21 -0300

    arreglo estructura y archivos para nuevo orden limpio ejs

--- src/routes/vistas.js
+++ src/routes/vistas.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const path = require('path');
-const router = express.Router();
+const router = express.Router(); // ¡Ya no necesitamos requerir 'path'!
 
-// Ruta para el Home (index.html)
+// Ruta para el Home (busca index.ejs)
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.render('index');
 });
 
-// Ruta para el Login (login.html)
+// Ruta para el Login (busca login.ejs)
 router.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/login.html'));
+    res.render('login');
 });
 
-// Ruta para el Comparador (comparador.html)
+// Ruta para el Comparador (busca comparador.ejs)
 router.get('/comparador', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/comparador.html'));
+    res.render('comparador');
 });
 
-// Ruta para el Foro (foro.html)
+// Ruta para el Foro (busca foro.ejs)
 router.get('/foros', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/foro.html'));
+    res.render('foro');
 });
 
 module.exports = router;
